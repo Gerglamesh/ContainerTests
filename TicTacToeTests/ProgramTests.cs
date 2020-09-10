a8c0eb22894cf252e40bfb7b4b774aa893a247d5
Gerglamesh <hampe.kjellstrand@gmail.com>
2020-09-10 10:19:26 +0200

    Add project

---
+++ TicTacToeTests/ProgramTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicTacToe;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void Testtest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
